c5c757e46062fd26c3c3968245cc6dbe15165c87
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2023-05-22 12:55:47 +0200

    fixes

--- public/js/tuinieren-script.js
+++ public/js/tuinieren-script.js
@@ -1,63 +1,110 @@
-const overlap = "50%"
+const overlap = "50%";
 
 Draggable.create(".paintbrush", {
-    bounds:"body",
-    onDrag: function(){
-        const dropArea = document.querySelector('.wood')
-        if(this.hitTest(dropArea, overlap)){
-            (this.target).classList.add("dropper");
-        }else{
-            (this.target).classList.remove("dropper");
-        }},
-    onDragEnd: function(){
-        const wood = document.querySelectorAll('.wood path')
-        if(this.target.classList.contains("dropper")){
-            wood.forEach(wood =>{
-                // wood.classList.add(".paint-fence")
-                wood.style.fill= "blue"
-                setTimeout(()=>{TweenLite.to(this.target, 0.2,{
-                    x:0,
-                    y:0,
-                })}, 1000)
-            })
-        }else{
-            TweenLite.to(this.target, 0.2,{
-                x:0,
-                y:0,
-            })
-        }
-    }    
+  bounds: "body",
+  onDrag: function (e) {
+    console.log(e);
+
+    const dropArea = document.querySelector(".wood");
+    if (this.hitTest(dropArea, overlap)) {
+      this.target.classList.add("dropper");
+    } else {
+      this.target.classList.remove("dropper");
+    }
+  },
+  onDragEnd: function () {
+    const wood = document.querySelectorAll(".wood path");
+    if (this.target.classList.contains("dropper")) {
+      wood.forEach((wood) => {
+        wood.style.fill = "blue";
+        gsap.to(this.target, {
+          x: 0,
+          y: 0,
+          duration: 0.2,
+          delay: 1,
+        });
+      });
+    } else {
+      gsap.to(this.target, {
+        x: 0,
+        y: 0,
+        duration: 0.2,
+        delay: 1,
+      });
+    }
+  },
 });
 
 Draggable.create(".bird-seed", {
-    bounds:"body",
-    onDrag: function(){
-        const bird = document.querySelector(".bird")
-        const dropArea = document.querySelector(".birdhouse")
-        if(this.hitTest(dropArea, overlap)){
-            (this.target).classList.add("dropper");
-        }else{
-            (this.target).classList.remove("dropper");
-        }},
-    onDragEnd: function(){
-        const bird = document.querySelector(".bird")
-        if(this.target.classList.contains("dropper")){
-            bird.classList.add("bird-animation")
-            bird.addEventListener('animationend', () =>{
-            setTimeout(()=>{TweenLite.to(this.target, 0.2,{
-                x:0,
-                y:0,
-            })}, 1000)})
-            bird.addEventListener("animationend", () =>{
-                bird.classList.remove("bird-animation")
-            })
-
-        }else{
-            TweenLite.to(this.target, 0.2,{
-                x:0,
-                y:0,
-            })
-        }
-    }   
+  bounds: "body",
+  onDrag: function () {
+    const bird = document.querySelector(".bird");
+    const dropArea = document.querySelector(".birdhouse");
+    if (this.hitTest(dropArea, overlap)) {
+      this.target.classList.add("dropper");
+    } else {
+      this.target.classList.remove("dropper");
     }
-)
+  },
+  onDragEnd: function () {
+    const bird = document.querySelector(".bird");
+    if (this.target.classList.contains("dropper")) {
+      bird.classList.add("bird-animation");
+      bird.addEventListener("animationend", () => {
+        gsap.to(this.target, {
+          x: 0,
+          y: 0,
+          duration: 0.2,
+          delay: 1,
+        });
+
+        bird.addEventListener("animationend", () => {
+          bird.classList.remove("bird-animation");
+        });
+      });
+    } else {
+      gsap.to(this.target, {
+        x: 0,
+        y: 0,
+        duration: 0.2,
+      });
+    }
+  },
+});
+
+Draggable.create(".bone", {
+  bounds: "body",
+  onDrag: function () {
+    const dropArea = document.querySelector(".doghouse");
+    if (this.hitTest(dropArea, overlap)) {
+      this.target.classList.add("dropper");
+    } else {
+      this.target.classList.remove("dropper");
+    }
+  },
+
+  onDragEnd: function () {
+    const dog = document.querySelector(".dog");
+    if (this.target.classList.contains("dropper")) {
+      dog.classList.add("dog-animation");
+      dog.addEventListener("animationend", () => {
+        gsap.to(this.target, {
+          x: 0,
+          y: 0,
+          duration: 0.2,
+          delay: 1,
+        });
+
+        dog.addEventListener("animationend", () => {
+          dog.classList.remove("dog-animation");
+        });
+      });
+    } else {
+      gsap.to(this.target, {
+        x: 0,
+        y: 0,
+        duration: 0.2,
+      });
+    }
+  },
+});
